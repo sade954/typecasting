cb07e8a091b0bbff2d73c37df40df99d55d59319
Candace Hollinger <csig1985@gmail.com>
2022-12-10 23:36:45 +0000

    updated typecasting

---
+++ typecasting.py
@@ -0,0 +1,37 @@
+#Typecasting an integer
+str(1)
+'1'
+
+#Typecasting a float integer 
+str(1.0)
+'1.0'
+
+#False value
+str(False)
+'False'
+
+#Typecasting an integer/float
+int('1')
+1
+float('1')
+1.0
+float('1.2')
+1.2
+
+#Boolean true value
+bool(1)
+True
+bool('2.4')
+True
+
+#Boolean false value
+bool(0)
+False
+bool(0.0)
+False
+bool('')
+False
+
+#Returning false value
+1 and 0
+0
